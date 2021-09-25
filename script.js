baf18b214eacf901d133c66f0ce3f99ff8ebf6fd
Saicharan Gouru <gourusaicharan6@gmail.com>
2021-09-25 19:28:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ function profitOrLoss(initial, quantity, current) {
     output.style.color = "black";
     if (initial > current) {
         var loss = (initial - current) * quantity;
-        var lossPercentage = (loss / initial) * 100;
+        var lossPercentage = ((loss) / (initial * quantity)) * 100;
         output.style.color = "red";
         output.innerText = `Loss = ${loss} \n Loss Percentage = ${lossPercentage}%`;
 
     } else if (initial < current) {
         var profit = (current - initial) * quantity;
-        var profitPercentage = (profit / initial) * 100;
+        var profitPercentage = ((profit) / (initial * quantity)) * 100;
         output.style.color = "green";
         output.innerText = `Profit = ${profit} \n Profit Percentage = ${profitPercentage}%`;
 
